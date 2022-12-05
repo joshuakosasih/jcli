4fbc216f1053add612675e0f7c6394e896f36140
joshua.kosasih <joshua.kosasih@shopee.com>
2022-12-05 18:16:15 +0800

    various improvements: - added describe() and indexes() to peek better into data - added docstring to some methods - enable multiple keywords match

--- es.py
+++ es.py
@@ -2,7 +2,7 @@ import yaml
 import codecs
 from elasticsearch import Elasticsearch
 from opensearchpy import OpenSearch
-from elasticsearch_dsl import Search, Index, UpdateByQuery
+from elasticsearch_dsl import Search, Index, UpdateByQuery, Document
 
 
 class Profile:
@@ -35,6 +35,18 @@ class Profile:
 
 
 class Jes:
+    """
+    jES - a class to wrap and interact with ES dsl client
+
+    Popular methods:
+    - match(...) -> Search obj
+    - regexp(...) -> Search obj
+    - update(...) -> UpdateByQuery obj
+
+    Several methods has x{} counterparts which executes directly, eg:
+    - xmatch(...): match(...).execute() -> Response obj
+    """
+
     def __init__(self, cli, index="", size=10):
         self.client = cli
         self.idx_name = index
@@ -49,6 +61,21 @@ class Jes:
     def index(self) -> Index:
         return Index(self.idx_name, using=self.client)
 
+    def describe(self) -> dict:
+        """
+        Describe the mappings of the index (idx_name).
+        :usage: jes.describe() or jes.describe().keys()
+        """
+        mappings = Index(self.idx_name, using=self.client).get_mapping()[self.idx_name]["mappings"]["properties"]
+        return mappings
+
+    def indexes(self) -> dict:
+        """
+        Show indexes and their info
+        :usage: jes.indexes().keys() or jes.indexes()
+        """
+        return Index("", using=self.client).stats()["indices"]
+
     def refresh(self):
         self.index().refresh()
 
@@ -58,13 +85,25 @@ class Jes:
         return Search(using=self.client, index=self.idx_name).extra(size=self.size).source(select)
 
     def match(self, select: [str] = None, **where) -> Search:
-        return self.search(select).query("match", **where)
+        """
+        Returns search_object matching the parameter given
+        :usage: jes.match(["item_id", "name"], item_id=1234, shop_id=567)
+        """
+        return self._matcher(self.search(select), where)
 
     def regexp(self, select: [str] = None, **where) -> Search:
+        """
+        Returns search_object matching the given regex
+        :usage: jes.match(["item_id", "name"], item_id=".*\..*")
+        """
         return self.search(select).query("regexp", **where)
 
     def update(self, update_query: str, **where) -> UpdateByQuery:
-        return UpdateByQuery(using=self.client, index=self.idx_name).query("match", **where)\
+        """
+        Returns update_object matching the parameter given
+        :usage: jes.update("status=0", item_id=1234, shop_id=567)
+        """
+        return self._matcher(UpdateByQuery(using=self.client, index=self.idx_name), where)\
             .script(source=f"ctx._source.{update_query}", lang="painless")
 
     def xmatch(self, select: [str] = None, **where):
@@ -75,6 +114,11 @@ class Jes:
 
     def xupdate(self, update_query: str, **where):
         return self.update(update_query, **where).execute()
+
+    def _matcher(self, obj, where: dict):
+        for k, v in where.items():
+            obj = obj.query("match", k=v)
+        return obj
 
 
 def get_es_cli(profile_name: str = None):
